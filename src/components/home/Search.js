0a7ec7cefe4e4b71323a3471bd24c0a6dccf4581
suqun zhang <susizhang2017@gmail.com>
2022-12-06 16:40:32 +0100

    Adding files

--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -1,16 +1,17 @@
 import { BiSearchAlt2 } from "react-icons/bi";
+import logo from "../../img/logo 2.png";
 
-function Search({ pokemons }) {
+function Search() {
   return (
     <>
-      <div className="grid grid-cols-8 gap-5 flex flex-col items-center justify-center ">
-        <h1 className="col-span-2 text-5xl text-white font-bold ">Pokéfight</h1>
-        <div className="col-span-6">
+      <div className="grid grid-cols-8 gap-10 flex flex-col items-center justify-center ">
+        <img className="col-span-2" src={logo} alt="" />
+        <div className="col-span-5">
           <form action="">
             <input
-              className="border border-zinc-800"
+              className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
               type="text"
-              placeholder="Name or Number"
+              placeholder="write name or number to search a pokemon"
             />
             <button className="text-white bg-orange-500 hover:bg-orange-700 text-xl">
               <BiSearchAlt2 />
